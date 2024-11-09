789b76a6198d7a6aef031e3bf6039ad8028e1c6c
Ikramul Hasan Rakib <hasanrakib3590@gmail.com>
2024-11-09 20:55:18 +0600

    updated url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-commerce-client-rosy.vercel.app",
     credentials: true,
   })
 );
